2afbd1427242525a2d6211a2ccb768778d1213e0
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-01-24 23:59:40 +0100

    Reworked navigation (key to mouse)

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,76 +13,39 @@ public class Controller : MonoBehaviour
     [SerializeField] KeyCode UpKey;
     [SerializeField] KeyCode DownKey;
     [SerializeField, Range(0, 1)] float translateSpeed;
-    [SerializeField, Range(0, 1)] float rotationSpeed = 1; 
-    [SerializeField, Range(0, 1)] float zoomSpeed = 1;
+    [SerializeField, Range(0, 1)] float rotationSpeed; 
+    [SerializeField, Range(0, 1)] float zoomSpeed;
 
 
     void OnGUI()
     {
-      
+
         #region moveGO
-        
-        // Rotating
-        if (Input.GetKey("q"))
-        {
-
-            if (Input.GetKey(RightKey))
-            {
-                paintObject.transform.Rotate((new Vector3(0, 1, 0)) * rotationSpeed, Space.World);
-            }
-            else if (Input.GetKey(LeftKey))
-            {
-                paintObject.transform.Rotate((new Vector3(0, -1, 0)) * rotationSpeed, Space.World);
-            }
-
-            else if (Input.GetKey(UpKey))
-            {
-                paintObject.transform.Rotate((new Vector3(1, 0, 0)) * rotationSpeed, Space.World);
-            }
-
-
-            else if (Input.GetKey(DownKey))
-            {
-                paintObject.transform.Rotate((new Vector3(-1, 0, 0)) * rotationSpeed, Space.World);
-            }
-
-        }
 
         // Zooming
-        else if (Input.GetKey("e"))
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            if (Input.GetKey(DownKey))
-            {
-                Camera.transform.Translate((new Vector3(0, 0, -1)) * 0.1f * zoomSpeed, Space.World);
-            }
-            else if (Input.GetKey(UpKey))
-            {
-                Camera.transform.Translate((new Vector3(0, 0, 1)) * 0.1f * zoomSpeed, Space.World); ;
-            }
+            Camera.transform.Translate((new Vector3(0, 0, -1)) * 0.1f * zoomSpeed, Space.World);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            Camera.transform.Translate((new Vector3(0, 0, 1)) * 0.1f * zoomSpeed, Space.World);
+        }
+
+        // Rotating
+        if (Input.GetMouseButton(1))
+        {
+            paintObject.transform.Rotate((new Vector3(0, -1, 0)) * rotationSpeed * Input.GetAxis("Mouse X"), Space.World);
+            paintObject.transform.Rotate((new Vector3(1, 0, 0)) * rotationSpeed * Input.GetAxis("Mouse Y"), Space.World);
         }
 
         // Translating
-        else
+        else if (Input.GetMouseButton(2))
         {
-            if (Input.GetKey(RightKey))
-            {
-                paintObject.transform.Translate((new Vector3(1, 0, 0)) * 0.1f * translateSpeed, Space.World);
-            }
-            else if (Input.GetKey(LeftKey))
-            {
-                paintObject.transform.Translate((new Vector3(-1, 0, 0)) * 0.1f * translateSpeed, Space.World);
-            }
-            else if (Input.GetKey(UpKey))
-            {
-                paintObject.transform.Translate((new Vector3(0, 1, 0)) * 0.1f * translateSpeed, Space.World);
-            }
-            else if (Input.GetKey(DownKey))
-            {
-                paintObject.transform.Translate((new Vector3(0, -1, 0)) * 0.1f * translateSpeed, Space.World);
-            }
-
+            paintObject.transform.Translate((new Vector3(1, 0, 0)) * 0.1f * translateSpeed * Input.GetAxis("Mouse X"), Space.World);
+            paintObject.transform.Translate((new Vector3(0, 1, 0)) * 0.1f * translateSpeed * Input.GetAxis("Mouse Y"), Space.World);
         }
-        
+
         #endregion
 
     }
